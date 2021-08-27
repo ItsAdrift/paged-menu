ae596ed761b8d6f9898aa95fe87f93823c1b30f3
ItsAdrift <68416448+ItsAdrift@users.noreply.github.com>
2021-08-27 17:20:11 +1000

    Update MenuPage.java

--- MenuPage.java
+++ MenuPage.java
@@ -1,13 +1,9 @@
-package me.itsadrift.mooseclub.menu.paged;
+package me.itsadrift.pagedmenu;
 
-import me.itsadrift.mooseclub.menu.MenuButton;
-import me.itsadrift.mooseclub.utils.HexUtils;
-import me.itsadrift.mooseclub.utils.ItemBuilder;
+import me.itsadrift.pagedmenu.menu.MenuButton;
+
 import org.bukkit.Bukkit;
-import org.bukkit.Material;
 import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.SkullMeta;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -24,7 +20,7 @@ public class MenuPage {
 
         this.itemMap = (HashMap<Integer, MenuButton>) items.clone();
 
-        inventory = Bukkit.createInventory(null, 54, HexUtils.colour(title));
+        inventory = Bukkit.createInventory(null, 54, colour(title));
         for (Map.Entry<Integer, MenuButton> e : itemMap.entrySet()) {
             inventory.setItem(e.getKey(), e.getValue().getItemStack());
         }
@@ -41,4 +37,9 @@ public class MenuPage {
     public HashMap<Integer, MenuButton> getItemMap() {
         return itemMap;
     }
+    
+    private String colour(String s) {
+        return ChatColor.translateAlternateColorCodes('&', s);
+    }
+    
 }
